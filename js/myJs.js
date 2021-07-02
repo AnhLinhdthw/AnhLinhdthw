8d6577002b4b624b668b26c1951a051c3e33d2fd
AnhLinhdthw <86816345+AnhLinhdthw@users.noreply.github.com>
2021-07-02 18:17:40 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -28,7 +28,7 @@ function firstQuestion(){
 
  // switch button position
  function switchButton() {
-     var audio = new Audio('sound/crush.mp3');
+     var audio = new Audio('sound/switch.mp3');
     audio.play();
     var leftNo = $('#no').css("left");
     var topNO = $('#no').css("top");
